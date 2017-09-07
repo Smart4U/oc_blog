41aecf6acd98411e6c8a7160c5a01814175b042c
Smart4U <email4webdev@gmail.com>
2017-09-07 15:43:56 +0200

    Add migration for database

---
+++ database/migrations/20170907095409_create_posts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreatePostsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('posts')
+            ->addColumn('category_id', 'integer', [
+                'null' => true
+            ])
+            ->addForeignKey('category_id', 'categories', 'id', [
+                'delete' => 'SET_NULL',
+                'update' => 'NO_ACTION'
+            ])
+            ->addColumn('author', 'string')
+            ->addColumn('slug', 'string')
+            ->addColumn('title', 'string')
+            ->addColumn('subtitle', 'string')
+            ->addColumn('content', 'text', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::TEXT_LONG])
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addColumn('published_at', 'datetime')
+            ->addColumn('online', 'integer', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
+            ->create();
+    }
+}
