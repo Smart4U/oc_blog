5658ef56dd039c6fcf6642a097f23b61002e5ae0
Smart4U <email4webdev@gmail.com>
2017-09-07 16:31:57 +0200

    Add HTTP Request

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
 require dirname(__DIR__) . '/bootstrap.php';
+
+
+$request = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
+
+$response = new \GuzzleHttp\Psr7\Response(
+    200,
+    ['x-powered-by' => 'oc_blog'],
+    null,
+    1.1
+);
