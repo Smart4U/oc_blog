ecbac873ec0a4a1f04238708e7a6d1490dc3ab3c
Smart4U <email4webdev@gmail.com>
2017-09-07 15:36:56 +0200

    Add the minimum project configuration

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,23 @@
 <?php
 
 require 'vendor/autoload.php';
+
+
+/**
+ * Constantes of project
+ */
+define('ROOT', __DIR__);
+define('CONFIG', ROOT.'/config/');
+
+
+/**
+ * Symfony Dotenv
+ */
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(ROOT.'/.env.prod', ROOT.'/.env.dev');
+
+
+/**
+ * The configuration
+ */
+$config = \App\Core\Config::getInstance(CONFIG);
---
+++ src/Core/Config.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Class Config
+ * This class makes it easy to retrieve an element of the configuration
+ */
+namespace App\Core;
+
+final class Config {
+
+    /**
+     * @var array
+     */
+    private $settings = [];
+
+    /**
+     * @var Config
+     */
+    private static $_instance;
+
+    /**
+     * Config constructor.
+     * @param string $path
+     */
+    private function __construct(string $path)
+    {
+        if(file_exists($path)){
+            $config = [];
+            $files = glob(sprintf($path.'/{{,*.}global,{,*.}%s}.php', getenv('APP_ENV') ? : 'prod'), GLOB_BRACE);
+
+            foreach ($files as $file) {
+                $this->settings = array_merge($config, include $file);
+            }
+
+        }
+    }
+
+    /**
+     * @param string $path
+     * @return Config
+     */
+    public static function getInstance(string $path) :Config
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Config($path);
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * @param string $key
+     * @return null|string
+     */
+    public function get(string $key) :?string
+    {
+        if (!isset($this->settings[$key])) {
+            return null;
+        }
+        return $this->settings[$key];
+    }
+
+}
